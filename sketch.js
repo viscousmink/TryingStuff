a40a21fbd23eb7603576a0c9e5751d5404fbb057
viscousmink <mitchell.klingler99@gmail.com>
2019-06-12 14:48:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 
 var cols, rows;
-var scl = 20;
-var w = 1800;
+var scl = 20
+var w = 1400;
 var h = 1000;
 var Yflying = 0;
 var Xflying = 0;
 var terrain = [];
+var startMode = true;
 
 function setup() {
   createCanvas(1400, 800, WEBGL);
@@ -20,16 +21,26 @@ function setup() {
   }
 }
 
+function changeMode() {
+  startMode = false;
+}
+
 function draw() {
-  if(keyIsPressed == true) {
+  
+  if(keyIsPressed == true || startMode == true) {
+	if(keyIsPressed == true) {
+	  changeMode();
+	}
     if(key == 'w') {
 	  Yflying -= 0.1;
     } else if(key == 's') {
 	  Yflying += 0.1;
-	} else if(key == 'a') {
-	  Xflying -= 0.1;
-	} else if(key == 'd') {
-	  Xflying += 0.1;
+    } else if(key == 'a') {
+      Xflying -= 0.1;
+    } else if(key == 'd') {
+      Xflying += 0.1;
+    } else if(startMode == true) {
+      Yflying -= 0.1;
 	}
   
     var yoff = Yflying;
@@ -44,8 +55,9 @@ function draw() {
 
     background(0);
     translate(0, 50);
-    rotateX(PI/2.5);
+    rotateX(PI/2.7);
     fill(200,200,200, 50);
+	//fill(255);
     translate(-w/2, -h/2);
     for (var y = 0; y < rows-1; y++) {
       beginShape(TRIANGLE_STRIP);
